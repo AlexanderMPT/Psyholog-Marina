fe26db3a29e9ac27b35dc7dbe0128152929f445a
Александр Чикин <106692559+AlexanderMPT@users.noreply.github.com>
2023-10-22 17:08:27 +0300

    Add files via upload

---
+++ js/pop-up.js
@@ -0,0 +1,17 @@
+// Obtain the necessary DOM elements
+const button = document.querySelector('.first__wrapper-inner');
+const popup = document.querySelector('.form');
+
+// Add an event listener for the button click
+button.addEventListener('click', () => {
+  // Slide the pop-up window down by changing the transform property
+  popup.style.transform = 'translateY(0)';
+});
+
+// Add an event listener to close the pop-up window when clicking outside of it
+document.addEventListener('click', (event) => {
+  if (!event.target.closest('.form') && !event.target.closest('.first__wrapper-inner')) {
+    // Slide the pop-up window back up and hide it
+    popup.style.transform = 'translateY(-200%)';
+  }
+});
